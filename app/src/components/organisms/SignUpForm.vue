82d4dd79f8c0e38f3c511d30c818510d053f9a26
Shingo Tsuruta <gmT.S8.531@gmail.com>
2021-10-05 18:06:50 +0900

    Split SignUp to SignUpForm

---
+++ app/src/components/organisms/SignUpForm.vue
@@ -0,0 +1,29 @@
+<template>
+<form>
+  <label for="name">ユーザー名</label>
+  <input type="text" id="name" v-model="name">
+  <label for="email">メールアドレス</label>
+  <input type="email" id="email" v-model="email">
+  <label for="password">パスワード</label>
+  <input type="password" id="password" v-model="password">
+  <button type="button" @click="signUp">新規登録</button>
+</form>
+</template>
+
+<script>
+export default {
+  name: 'SignUpForm',
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    signUp() {
+      console.log('signUp')
+    }
+  }
+}
+</script>
