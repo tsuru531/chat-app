d7b4e8d01bdb0ebe1e5e7f017b85dfb3535c33cb
Shingo Tsuruta <gmT.S8.531@gmail.com>
2021-10-13 17:47:19 +0900

    Add feature allows go to myPage only when login

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Store from '@/store'
+import { auth, db } from '@/firebase'
+import { onAuthStateChanged } from 'firebase/auth'
+import { collection, doc, getDoc } from 'firebase/firestore'
 
 Vue.use(VueRouter)
 
@@ -22,7 +26,8 @@ const routes = [
   {
     path: '/dashboard',
     name: 'Dashboard',
-    component: () => import('@/pages/Dashboard')
+    component: () => import('@/pages/Dashboard'),
+    meta: { requiresAuth: true }
   }
 ]
 
@@ -30,6 +35,30 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  const isSignedIn = Store.getters['user/isSignedIn']
+  if (requiresAuth && !isSignedIn) {
+    onAuthStateChanged(auth, async user => {
+      if (user) {
+        const uid = user.uid
+        const collectionRef = collection(db, 'users')
+        const docRef = doc(collectionRef, uid)
+        const docSnapshot = await getDoc(docRef)
+        const data = docSnapshot.data()
+        Store.commit('user/signIn', {
+          uid,
+          name: data.name
+        })
+        next()
+      } else {
+        next('/signin')
+      }
+    })
+  }
+  next()
 })
 
 export default router
