5655bcc430e94647e4b88dbdc2b6412d406780fd
Shingo Tsuruta <gmT.S8.531@gmail.com>
2021-10-30 20:34:46 +0900

    Stylel ThreadsList component

--- app/src/components/organisms/ThreadsList.vue
+++ app/src/components/organisms/ThreadsList.vue
@@ -1,14 +1,21 @@
 <template>
 <ul>
-  <li v-for="(thread) in threads" :key="thread.id">
-    <router-link :to="{ path: `/thread/${thread.id}` }">{{ thread.title }}</router-link>
+  <li v-for="{ id, title } in threads" :key="id">
+    <router-link :to="{ path: `/thread/${id}` }">
+      <ThreadCard :title="title" />
+    </router-link>
   </li>
 </ul>
 </template>
 
 <script>
+import ThreadCard from '@/components/molecules/ThreadCard'
+
 export default {
   name: 'ThreadsList',
+  components: {
+    ThreadCard
+  },
   computed: {
     threads() {
       return this.$store.getters['thread/threads']
@@ -19,3 +26,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+li {
+  list-style: none;
+}
+ul {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  padding: 16px;
+  margin: 0;
+}
+</style>
