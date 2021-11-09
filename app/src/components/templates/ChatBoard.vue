cdfd7447ce7246190d53f59688c8e62abbcb77d0
Shingo Tsuruta <gmT.S8.531@gmail.com>
2021-11-09 19:17:12 +0900

    Use MainContainer in ChatBoard

--- app/src/components/templates/ChatBoard.vue
+++ app/src/components/templates/ChatBoard.vue
@@ -1,14 +1,19 @@
 <template>
 <div>
   <Header />
-  <h1>{{ title }}</h1>
-  <router-link :to="{ path: `/thread/${threadId}/menu` }">メニュー</router-link>
-  <CommentsList />
-  <ResponseForm />
+  <section>
+    <MainContainer>
+      <h1>{{ title }}</h1>
+      <router-link :to="{ path: `/thread/${threadId}/menu` }">メニュー</router-link>
+      <CommentsList />
+      <ResponseForm />
+    </MainContainer>
+  </section>
 </div>
 </template>
 
 <script>
+import MainContainer from '@/components/atoms/MainContainer'
 import Header from '@/components/organisms/Header'
 import CommentsList from '@/components/organisms/CommentsList'
 import ResponseForm from '@/components/organisms/ResponseForm'
@@ -16,6 +21,7 @@ import ResponseForm from '@/components/organisms/ResponseForm'
 export default {
   name: 'ChatBoard',
   components: {
+    MainContainer,
     Header,
     CommentsList,
     ResponseForm
@@ -26,3 +32,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+section {
+  display: flex;
+  justify-content: center;
+}
+</style>
