c75892b2129c7b08e8cc47064a946b537d605384
Shingo Tsuruta <gmT.S8.531@gmail.com>
2021-11-09 19:11:28 +0900

    Create MainContainer component

---
+++ app/src/components/atoms/MainContainer.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <slot></slot>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'MainContainer'
+}
+</script>
+
+<style scoped>
+div {
+  width: 100%;
+  max-width: 680px;
+}
+</style>
