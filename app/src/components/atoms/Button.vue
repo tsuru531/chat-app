fbbc55addd93cbb5f519f3631cd087c967de89e2
Shingo Tsuruta <gmT.S8.531@gmail.com>
2021-11-22 19:59:05 +0900

    Create Button component

---
+++ app/src/components/atoms/Button.vue
@@ -0,0 +1,54 @@
+<template>
+<button type="button" @click.stop="click" :class="classObject">
+  <span>{{ label }}</span>
+</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    label: String,
+    color: {
+      type: String,
+      require: false
+    }
+  },
+  computed: {
+    classObject() {
+      return {
+        cp: this.color === 'primary',
+        cd: this.color === 'danger'
+      }
+    }
+  },
+  methods: {
+    click() {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  padding: 8px 16px;
+  appearance: none;
+  border-radius: 4px;
+  font-size: 14px;
+  font-family: Roboto;
+  font-weight: normal;
+  letter-spacing: 1.25px;
+}
+button.cp {
+  background-color: #4676D7;
+}
+button.cd {
+  background-color: #dc3545;
+  color: #eee;
+}
+</style>
