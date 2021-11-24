c326b218f74f4553a04c6f355529820867a95d4d
Shingo Tsuruta <gmT.S8.531@gmail.com>
2021-11-24 16:40:34 +0900

    Update CommentsList component

--- app/src/components/organisms/CommentsList.vue
+++ app/src/components/organisms/CommentsList.vue
@@ -1,22 +1,66 @@
 <template>
-<ul>
-  <li v-for="(comment) in comments" :key="comment.id">
-    <CommentItem :comment="comment" />
-  </li>
-</ul>
+<div>
+  <ul>
+    <li v-for="(comment) in comments" :key="comment.id">
+      <CommentItem :comment="comment" @deleteItem="displayModal(comment)" />
+    </li>
+  </ul>
+  <ModalDialog v-if="modal.isDisplayed" @close="hideModal">
+    <template v-slot:content>
+      <p>このコメントを削除してもよろしいですか？</p>
+      <p>{{ modal.content }}</p>
+    </template>
+    <template v-slot:footer>
+      <div class="button-wrapper">
+        <Button label="いいえ" @click="hideModal" />
+        <Button label="はい" color="danger" @click="deleteComment" />
+      </div>
+    </template>
+  </ModalDialog>
+</div>
 </template>
 
 <script>
+import Button from '@/components/atoms/Button'
 import CommentItem from '@/components/molecules/CommentItem'
+import ModalDialog from '@/components/molecules/ModalDialog'
 
 export default {
   name: 'CommentsList',
   components: {
-    CommentItem
+    Button,
+    CommentItem,
+    ModalDialog
+  },
+  data() {
+    return {
+      modal: {
+        isDisplayed: false,
+        comment_id: '',
+        content: ''
+      }
+    }
   },
   computed: {
     comments() {
       return this.$store.getters['thread/comments']
+    }
+  },
+  methods: {
+    displayModal(comment) {
+      const { id, content } = comment
+      this.modal.isDisplayed = true
+      this.modal.comment_id = id
+      this.modal.content = content
+    },
+    hideModal() {
+      this.modal.isDisplayed = false
+      this.modal.comment_id = ''
+      this.modal.content = ''
+    },
+    deleteComment() {
+      this.$store.dispatch('thread/deleteComment', this.modal.comment_id)
+      this.hideModal()
     }
   }
 }
@@ -32,5 +76,9 @@ ul {
 }
 li {
   list-style: none;
+}
+.button-wrapper {
+  display: flex;
+  gap: 8px;
 }
 </style>
