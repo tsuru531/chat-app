3b9606219ab0e87142a9157b0577f028ff64dea1
Shingo Tsuruta <gmT.S8.531@gmail.com>
2022-01-26 17:46:59 +0900

    Create search module

--- app/src/store/modules/threads/search/index.js
+++ app/src/store/modules/threads/search/index.js
@@ -2,15 +2,12 @@ import { state } from './state';
 import { getters } from './getters';
 import { mutations } from './mutations';
 import { actions } from './actions';
-import { search } from './search';
 
-export const threads = {
+export const search = {
   namespaced: true,
   state,
   getters,
   mutations,
   actions,
-  modules: {
-    search
-  }
+  modules: {}
 };
