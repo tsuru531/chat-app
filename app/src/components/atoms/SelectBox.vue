382c585871b4ce9f22b7bb18b1014b620fe34430
Shingo Tsuruta <gmT.S8.531@gmail.com>
2022-03-22 20:23:35 +0900

    Create SelectBox component

---
+++ app/src/components/atoms/SelectBox.vue
@@ -0,0 +1,32 @@
+<template>
+<select :name="name" @change="updateValue">
+  <template v-for="(option, index) in options">
+    <option :value="option.value" :key="index">
+      {{ option.label }}
+    </option>
+  </template>
+</select>
+</template>
+
+<script>
+export default {
+  name: 'SelectBox',
+  props: {
+    value: { type: String, required: true },
+    options: { type: Array, required: true },
+    name: { type: String, required: true },
+  },
+  methods: {
+    updateValue(e) {
+      this.$emit("input", e.target.value)
+    },
+  },
+  mounted() {
+    this.$emit("input", this.options[0].value)
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
