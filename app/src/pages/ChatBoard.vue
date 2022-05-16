91815cfd0635ebac9a271cc66bd5a3cd03867f4b
Shingo Tsuruta <gmT.S8.531@gmail.com>
2022-05-16 19:04:12 +0900

    Style ChatBoard

--- app/src/pages/ChatBoard.vue
+++ app/src/pages/ChatBoard.vue
@@ -1,19 +1,23 @@
 <template>
 <div class="chatboard">
   <Header />
-  <div class="chatboard-thread">
-    <Thread v-if="isLoaded" @reply="reply" />
-    <Loading v-else />
+  <div class="chatboard-thread_wrapper">
+    <div class="chatboard-thread">
+      <Thread v-if="isLoaded" @reply="reply" />
+      <Loading v-else />
+    </div>
   </div>
-  <div class="chatboard-form">
-    <ResponseForm
-      ref="response_form"
-      :response="response"
-      :handlename="handlename"
-      @change_response="changeResponse"
-      @change_handlename="changeHandlename"
-      @send="sendComment"
-    />
+  <div class="chatboard-form_wrapper">
+    <div class="chatboard-form">
+      <ResponseForm
+        ref="response_form"
+        :response="response"
+        :handlename="handlename"
+        @change_response="changeResponse"
+        @change_handlename="changeHandlename"
+        @send="sendComment"
+      />
+    </div>
   </div>
 </div>
 </template>
@@ -80,12 +84,28 @@ export default {
   width: 100%;
   height: 100vh;
 }
-.chatboard-thread {
+.chatboard-thread_wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   flex: 1;
   overflow-y: scroll;
+  width: 100%;
+}
+.chatboard-thread {
+  box-sizing: border-box;
+  width: min(100%, 760px);
   padding: 16px 20px 16px;
 }
+.chatboard-form_wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
 .chatboard-form {
+  box-sizing: border-box;
+  width: min(100%, 760px);
   padding: 0 16px 32px;
 }
 </style>
