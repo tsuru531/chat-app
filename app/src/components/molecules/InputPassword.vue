337f8843d3f90eeed8fa8e4f1c0cdd211d797810
Shingo Tsuruta <gmT.S8.531@gmail.com>
2022-06-03 09:34:33 +0900

    Add switch the display of the password

--- app/src/components/molecules/InputPassword.vue
+++ app/src/components/molecules/InputPassword.vue
@@ -1,7 +1,8 @@
 <template>
 <div>
   <Label>パスワード</Label>
-  <input name="password" id="password" type="text" @input="input">
+  <input name="password" id="password" :type="isDisplayed ? 'text' : 'password'" @input="input">
+  <button type="button" @click="switchDisplay">{{ isDisplayed ? '非表示' : '表示' }}</button>
 </div>
 </template>
 
@@ -13,12 +14,20 @@ export default {
   components: {
     Label,
   },
+  data() {
+    return {
+      isDisplayed: false,
+    }
+  },
   props: {
     value: { type: String, required: true },
   },
   methods: {
     input: function(e) {
       this.$emit('input', e.target.value);
+    },
+    switchDisplay: function() {
+      this.isDisplayed = !this.isDisplayed
     },
   },
 }
