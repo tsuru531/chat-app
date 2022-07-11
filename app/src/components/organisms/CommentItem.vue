d58eab2bb4f1a5a8f63e055fea36e6c1c388aecd
Shingo Tsuruta <gmT.S8.531@gmail.com>
2022-07-11 17:04:25 +0900

    Style CommentItem

--- app/src/components/organisms/CommentItem.vue
+++ app/src/components/organisms/CommentItem.vue
@@ -119,10 +119,12 @@ export default {
   vertical-align: top;
   box-sizing: border-box;
   border: solid 1px rgba(0, 0, 0, .4);
-  border-radius: 16px;
+  border-radius: 8px;
   margin: 0;
   padding: 4px 8px;
   white-space: pre-wrap;
+  min-width: 112px;
+  min-height: 34px;
 }
 .comment-item.body {
   display: flex;
