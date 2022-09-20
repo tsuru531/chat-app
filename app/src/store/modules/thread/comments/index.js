63f6571a9477282a780065b834b1bdd00d429b8e
tsuru531 <gmT.S8.531@gmail.com>
2022-09-20 17:01:37 +0900

    Create "comments" store

--- app/src/store/modules/thread/comments/index.js
+++ app/src/store/modules/thread/comments/index.js
@@ -2,17 +2,13 @@ import { state } from './state'
 import { getters } from './getters'
 import { mutations } from './mutations'
 import { actions } from './actions'
-import { comments } from './comments'
-import { likes } from './likes'
 
-export const thread = {
+export const comments = {
   namespaced: true,
   state,
   getters,
   mutations,
   actions,
   modules: {
-    comments,
-    likes,
   },
 }
