f292977fe1a01272ee98140ccb8808933edf5fb8
tsuru531 <gmT.S8.531@gmail.com>
2022-12-14 11:02:24 +0900

    Change watch comments feature

--- app/src/modules/watchComments.js
+++ app/src/modules/watchComments.js
@@ -1,13 +1,14 @@
 import { db } from '@/firebase';
-import { collection, doc, onSnapshot } from 'firebase/firestore';
+import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
 
 export async function watchComments(threadId, callback) {
-  const docRef = doc(collection(db, 'threads', threadId, 'comments'), '0');
+  const collectionRef = collection(db, 'threads', threadId, 'comments');
+  const q = query(collectionRef, orderBy('index', 'desc'));
   try {
-    const unsubscribe = await onSnapshot(docRef, (doc) => {
-      const data = doc.data();
-      const comments = data.list;
-      callback(comments);
+    const unsubscribe = await onSnapshot(q, (snapshot) => {
+      let list = [];
+      snapshot.forEach(doc => list = [...list, doc.data()]);
+      callback(list);
     });
     return unsubscribe;
   } catch (e) {
