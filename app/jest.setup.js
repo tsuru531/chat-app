4c02cc7374c7e224b100c602c7c40a479581e606
Shingo Tsuruta <gmT.S8.531@gmail.com>
2023-02-17 23:03:56 +0900

    Add RouterLinkMock to jest.setup.js

--- app/jest.setup.js
+++ app/jest.setup.js
@@ -1,4 +1,6 @@
 import { config } from '@vue/test-utils';
 import PortalMock from './__tests__/unit/PortalMock';
+import RouterLinkMock from './__tests__/unit/RouterLinkMock';
 
 config.stubs['portal'] = PortalMock;
+config.stubs['router-link'] = RouterLinkMock;
